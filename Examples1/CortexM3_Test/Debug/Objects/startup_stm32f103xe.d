b7d7a5b3ec5a6517570cbc3ae70135b35957378c
libertyzx <libertyzx@163.com>
2025-12-01 01:41:32 +0800

    修正windows中大小写问题-2

--- Examples1/CortexM3_Test/Debug/Objects/startup_stm32f103xe.d
+++
@@ -1,1 +0,0 @@
-.\debug\objects\startup_stm32f103xe.o: Code\Lib\STM32Cube_FW_F1\Drivers\CMSIS\Device\ST\STM32F1xx\Source\Templates\arm\startup_stm32f103xe.s
--- Examples1/CortexM3_Test/Debug/Objects/xc_list.d
+++
@@ -1,2 +0,0 @@
-.\debug\objects\xc_list.o: ..\..\Code\XC_List.c
-.\debug\objects\xc_list.o: ..\..\code\include\Internal/XC_List.h
